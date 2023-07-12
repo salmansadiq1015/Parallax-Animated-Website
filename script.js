58d8a3b2a30714bc77da39eb2b672fd00c4aaea6
Salman Sadiq <118099469+salmansadiq1015@users.noreply.github.com>
2023-07-12 10:30:28 -0700

    Parallax-Animated-Website
    Parallax-Animated-Website

---
+++ script.js
@@ -0,0 +1,16 @@
+const text = document.getElementById("text");
+const leaf = document.getElementById("leaf");
+const hill1 = document.getElementById("hill1");
+const hill4 = document.getElementById("hill4");
+const hill5 = document.getElementById("hill5");
+
+window.addEventListener("scroll", () => {
+  const value = window.scrollY;
+
+  text.style.marginTop = value * 2.5 + "px";
+  leaf.style.top = value * -1.5 + "px";
+  leaf.style.left = value * 1.5 + "px";
+  hill4.style.left = value * -1.5 + "px";
+  hill5.style.left = value * 1.5 + "px";
+  hill1.style.top = value * 1 + "px";
+});
